99b3c9af1a2d0f97037b149a2fe59dadb8cc642f
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-08-29 20:09:15 +0100

    Added UTC format support for Yewbacca. Drycoded.

--- Core.lua
+++ Core.lua
@@ -1,5 +1,5 @@
 
-local db, localTime, realmTime, displayedTime
+local db, localTime, realmTime, utcTime, displayedTime
 
 uClock = AceLibrary("AceAddon-2.0"):new("AceDB-2.0", "AceEvent-2.0", "FuBarPlugin-2.0")
 uClock.hasIcon = "Interface\\Icons\\INV_Misc_PocketWatch_02"
@@ -7,7 +7,7 @@ uClock.blizzardTooltip = true
 uClock.cannotHideText = true
 
 uClock:RegisterDB("uClockDB")
-uClock:RegisterDefaults('profile', { showLocal = true, showRealm = false, twentyFour = true, showSeconds = false, r = 1, g = 1, b = 1 })
+uClock:RegisterDefaults('profile', { showLocal = true, showRealm = false, showUTC = false, twentyFour = true, showSeconds = false, r = 1, g = 1, b = 1 })
 
 
 function uClock:OnEnable()
@@ -31,25 +31,32 @@ function uClock:OnEnable()
 				get = function() return db.showRealm end,
 				set = function() db.showRealm = not db.showRealm self:UpdateTimeStrings() end,
 			},
-			empty = { type = "header", order = 3 },
+			utcTime = {
+				name = "Show UTC Time",
+				desc = "Show UTC time on the FuBar.",
+				type = "toggle", order = 3,
+				get = function() return db.showUTC end,
+				set = function() db.showUTC = not db.showUTC self:UpdateTimeStrings() end,
+			},
+			empty = { type = "header", order = 4 },
 			twentyFour = {
 				name = "24 Hour Mode",
 				desc = "Choose whether to have the time shown in 12-hour or 24-hour format.",
-				type = "toggle", order = 4,
+				type = "toggle", order = 5,
 				get = function() return db.twentyFour end,
 				set = function() db.twentyFour = not db.twentyFour self:UpdateTimeStrings() end,
 			},
 			showSeconds = {
 				name = "Show Seconds",
 				desc = "Choose whether to show seconds.",
-				type = "toggle", order = 5,
+				type = "toggle", order = 6,
 				get = function() return db.showSeconds end,
 				set = function() db.showSeconds = not db.showSeconds self:UpdateTimeStrings() end,
 			},
 			colourOfText = {
 				name = "Colour of Text",
 				desc = "Choose the colour of the text.",
-				type = "color", order = 6,
+				type = "color", order = 7,
 				get = function() return db.r, db.g, db.b end,
 				set = function(r, g, b) db.r, db.g, db.b = r, g, b self:UpdateTimeStrings() end,
 			},
@@ -92,8 +99,10 @@ end
 function uClock:UpdateTimeStrings()
 	local lHour, lMinute = date("%H"), date("%M")
 	local sHour, sMinute = GetGameTime()
+	local uHour, uMinute = date("!%H"), date("!%M")
+	local seconds = date("%S")
 
-	local lPM, sPM
+	local lPM, sPM, uPM
 
 	if not db.twentyFour then
 		lPM = floor(lHour / 12) == 1
@@ -102,27 +111,37 @@ function uClock:UpdateTimeStrings()
 		sPM = floor(sHour / 12) == 1
 		sHour = mod(sHour, 12)
 
+		uPM = floor(uHour / 12) == 1
+		uHour = mod(uHour, 12)
+
 		if lHour == 0 then lHour = 12 end
 		if sHour == 0 then sHour = 12 end
+		if uHour == 0 then uHour = 12 end
 	end
 
-	localTime = ("%d:%02d"):format(lHour, lMinute)
-	realmTime = ("%d:%02d"):format(sHour, sMinute)
-
 	if db.showSeconds then
-		localTime = localTime..date(":%S")
-		realmTime = realmTime..date(":%S")
+		localTime = ("%d:%02d:%02d"):format(lHour, lMinute, seconds)
+		realmTime = ("%d:%02d:%02d"):format(sHour, sMinute, seconds)
+		utcTime   = ("%d:%02d:%02d"):format(uHour, uMinute, seconds)
+	else
+		localTime = ("%d:%02d"):format(lHour, lMinute)
+		realmTime = ("%d:%02d"):format(sHour, sMinute)
+		utcTime   = ("%d:%02d"):format(uHour, uMinute)
 	end
 
 	if not db.twentyFour then
 		localTime = localTime..(lPM and " PM" or " AM")
 		realmTime = realmTime..(sPM and " PM" or " AM")
+		utcTime   = utcTime .. (uPM and " PM" or " AM")
 	end
 
-	if db.showLocal and db.showRealm then displayedTime = localTime.." | "..realmTime
-	elseif db.showLocal then displayedTime = localTime
-	elseif db.showRealm then displayedTime = realmTime
-	else displayedTime = "" end
+	displayedTime = ""
+
+	if db.showLocal then displayedTime = displayedTime..localTime.." | " end
+	if db.showRealm then displayedTime = displayedTime..realmTime.." | " end
+	if db.showUTC then displayedTime = displayedTime..utcTime end
+
+	displayedTime = displayedTime:gsub(" | $", "") -- remove trailing seperator
 
 	self:SetText(("|cff%02x%02x%02x%s|r"):format(db.r*255, db.g*255, db.b*255, displayedTime))
 end
